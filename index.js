14f24447ddb091fa7d52598bd534132fb8892849
Lekia <lekiagospel@gmail.com>
2021-01-10 16:35:04 +0100

    add configuration file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const apiKey = config.OMDBAPIKey
 var request =  new XMLHttpRequest()
 
-request.open('GET',  'http://www.omdbapi.com/?t=Rush+hour&plot=full&apikey=dacfd155')
+request.open('GET',  `http://www.omdbapi.com/?t=Rush+hour&plot=full&apikey=${apiKey}`)
 request.onload = function() {
     var data = JSON.parse(this.response)
     console.log(data)
